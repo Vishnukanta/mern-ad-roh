d48220170c9ce43fa987ecbb4bc32041c2b8396f
MacBook Air <nazariipirih@MacBook-Air.local>
2023-06-11 20:54:04 +0300

    developing server part

---
+++ controllers/auth.js
@@ -0,0 +1,56 @@
+import bcript from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import User from '../models/User.js'
+
+// Register user
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body
+    const salt = await bcript.genSalt()
+    const passwordPath = await bcript.hash(password, salt)
+
+    const User = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordPath,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
+    })
+    const savedUser = await newUser.save()
+    res.status(201).json(savedUser)
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+}
+
+//Logging in
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body
+    const user = await User.finOne({ email: email })
+    if (!user) return res.status(400).json({ msg: 'User does not exist. ' })
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials. ' })
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+    delete user.password
+    res.status(200).json({token, user})
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+}
